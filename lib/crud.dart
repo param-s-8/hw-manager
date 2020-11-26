57d7307201b7a759ae8a3fdbf9ea1b714a36648f
Rasika Joshi <rasika2112@gmail.com>
2020-11-26 18:24:05 +0530

    Register, Login done

---
+++ lib/crud.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class crudMethods {
+  bool isLoggedIn() {
+    if (FirebaseAuth.instance.currentUser != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<void> addUser(userData) async {
+    if(isLoggedIn()) {
+      Firestore.instance.collection('users').add(userData).catchError((e) {
+        print(e);
+      });
+    } else {
+      print('You need to be logged in');
+
+    }
+  }
+}
---
+++ lib/register.dart
@@ -0,0 +1,147 @@
+import 'mainPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'crud.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _displayName = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _collegeController = TextEditingController();
+  final TextEditingController _subjectController = TextEditingController();
+
+  crudMethods crudObj = new crudMethods();
+  // ignore: unused_field
+  bool _isSuccess;
+  // ignore: unused_field
+  String _userEmail;
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Form(
+            key: _formKey,
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: _displayName,
+                      decoration: const InputDecoration(labelText: 'Full Name'),
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(labelText: 'Email'),
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: const InputDecoration(labelText: 'Password'),
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      obscureText: true,
+                    ),
+                    TextFormField(
+                      controller: _collegeController,
+                      decoration: const InputDecoration(labelText: 'College Name'),
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      obscureText: true,
+                    ),
+                    TextFormField(
+                      controller: _subjectController,
+                      decoration: const InputDecoration(labelText: 'Total no. of Subjects'),
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      obscureText: true,
+                    ),
+		
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      alignment: Alignment.center,
+                      child: OutlineButton(
+                        child: Text("register"),
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            _registerAccount();
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )),
+      ),
+    );
+  }
+
+  void _registerAccount() async {
+    final User user = (await _auth.createUserWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    ))
+        .user;
+
+    if (user != null) {
+      if (!user.emailVerified) {
+        await user.sendEmailVerification();
+      }
+      await user.updateProfile(displayName: _displayName.text);
+      final user1 = _auth.currentUser;
+      Map userData = {'Name': _displayName.text, 'Email': _emailController.text,'Password': _passwordController.text, 'College': _collegeController.text, 'Total Subjects': _subjectController.text};
+      crudObj.addUser(userData).then((result) {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => MainPage(
+              user: user1,
+            )));
+      }).catchError((e) {
+        print(e);
+      });
+
+    } else {
+      _isSuccess = false;
+    }
+  }
+}
