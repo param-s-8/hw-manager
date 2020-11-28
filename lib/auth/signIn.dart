a44067296de6d6199a68882eaa25ab18ec1e4276
Rasika Joshi <rasika2112@gmail.com>
2020-11-28 17:03:18 +0530

    Login UI

--- lib/auth/signIn.dart
+++ lib/auth/signIn.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'register.dart';
-import '../mainPage.dart';
 import '../screens/home.dart';
 
 class SignIn extends StatefulWidget {
@@ -15,78 +14,181 @@ class _SignInState extends State<SignIn> {
   final TextEditingController _emailController = TextEditingController();
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final TextEditingController _passwordController = TextEditingController();
+  double _boxh = 15;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      body: Builder(builder: (BuildContext context) {
-        return Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            withEmailPassword(),
-          ],
-        );
-      }),
-    );
-  }
-
-  Widget withEmailPassword() {
-    return Form(
-        key: _formKey,
-        child: Card(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
+      body: Stack(
+        children: <Widget>[
+          Scaffold(
+            backgroundColor: Colors.white,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('media/bg-app.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  child: const Text(
-                    'Sign in with email and password',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  alignment: Alignment.center,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.person_pin,
+                      size: 70,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      "LOGIN",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: _boxh*2),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(left:80,right:80),
+                          alignment: Alignment.centerLeft,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(30),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.white,
+                                  blurRadius: 6,
+                                  offset: Offset(0,2),
+                                )
+                              ]
+                          ),
+                          height: 50,
+                          child: TextFormField(
+                            controller: _emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                            decoration: InputDecoration(
+                              focusedBorder: InputBorder.none,
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black),
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              hintText: 'E-mail',
+                              prefixIcon: Icon(Icons.person,color: Colors.black,),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: _boxh),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(left:80,right:80),
+                          alignment: Alignment.centerLeft,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(30),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.white,
+                                  blurRadius: 6,
+                                  offset: Offset(0,2),
+                                )
+                              ]
+                          ),
+                          height: 50,
+                          child: TextFormField(
+                            controller: _passwordController,
+                            keyboardType: TextInputType.emailAddress,
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                            decoration: InputDecoration(
+                              focusedBorder: InputBorder.none,
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black),
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              hintText: 'Password',
+                              prefixIcon: Icon(Icons.lock,color: Colors.black,),
+                            ),
+                            obscureText: true,
+                          ),
+
+                        ),
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: FlatButton(
+                        onPressed: () => print("Forgot Password!"),
+                        padding: EdgeInsets.only(right:0),
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      width: double.infinity,
+                      margin: EdgeInsets.only(right: 150,left: 150),
+                      child: RaisedButton(
+                        elevation: 5,
+                        onPressed: () => _signInWithEmailAndPassword(),
+                        padding: EdgeInsets.all(15),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)
+                        ),
+                        color: Colors.white,
+                        child: Text(
+                          'LOGIN',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () => Register(),
+                      child: RichText(
+                        text: TextSpan(
+                            children: [
+                              TextSpan(
+                                  text: 'New User ? Register',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500,
+                                  )
+                              )
+                            ]
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(labelText: 'Email'),
-                  validator: (value) {
-                    if (value.isEmpty) return 'Please enter some text';
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: const InputDecoration(labelText: 'Password'),
-                  validator: (value) {
-                    if (value.isEmpty) return 'Please enter some text';
-                    return null;
-                  },
-                  obscureText: true,
-                ),
-                Container(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  alignment: Alignment.center,
-                  child: OutlineButton(
-                    child: Text("Sign In"),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        _signInWithEmailAndPassword();
-                      }
-                    },
-                  ),
-                ),
-                Container(
-                  child: OutlineButton(
-                    child: Text("Register"),
-                    onPressed: () => _pushPage(context, Register()),
-                  ),
-                  padding: const EdgeInsets.all(16),
-                  alignment: Alignment.center,
-                ),
+
               ],
             ),
           ),
-        ));
+        ],
+      ),
+    );
   }
 
   @override
