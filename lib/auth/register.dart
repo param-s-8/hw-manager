3b36d264a0fcb24945b5e5dbb67a3ad831c5a3b7
Rasika Joshi <rasika2112@gmail.com>
2020-11-28 16:11:50 +0530

    register UI

--- lib/auth/register.dart
+++ lib/auth/register.dart
@@ -14,103 +14,169 @@ class _RegisterState extends State<Register> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _displayName = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _collegeController = TextEditingController();
   final TextEditingController _subjectController = TextEditingController();
-
-  bool _isSuccess;
-  String _userEmail;
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
+  final TextEditingController _collegeController = TextEditingController();
+  double _boxh = 10;
 
   crudMethods crudObj = new crudMethods();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Form(
-            key: _formKey,
-            child: Card(
-              child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+      key: _scaffoldKey,
+      body: Stack(
+        children: <Widget>[
+          Scaffold(
+            backgroundColor: Colors.white,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('media/bg-app.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.create,
+                  size: 70,
+                  color: Colors.white,
+                ),
+                Text(
+                  "REGISTER",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: _boxh),
+                Container(
+                  height: 40,
+                  margin: EdgeInsets.only(left: 40, right: 40),
+                  child: TextFormField(
+                    controller: _displayName,
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'Full Name',
+                      prefixIcon: Icon(Icons.person, color: Colors.white),
+                      hintStyle: TextStyle(color: Colors.grey[700]),
+                    ),
+                  ),
+                ),
+                SizedBox(height: _boxh),
+                Container(
+                  height: 40,
+                  margin: EdgeInsets.only(left: 40, right: 40),
+                  child: TextFormField(
+                    controller: _emailController,
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      prefixIcon: Icon(Icons.email, color: Colors.white),
+                      hintStyle: TextStyle(color: Colors.grey[700]),
+                    ),
+                  ),
+                ),
+                SizedBox(height: _boxh),
+                Container(
+                  height: 40,
+                  margin: EdgeInsets.only(left: 40, right: 40),
+                  child: TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      prefixIcon: Icon(Icons.lock, color: Colors.white),
+                      hintStyle: TextStyle(color: Colors.grey[700]),
+                    ),
+                  ),
+                ),
+                SizedBox(height: _boxh),
+                Container(
+                  height: 40,
+                  margin: EdgeInsets.only(left: 40, right: 40),
+                  child: TextFormField(
+                    controller: _collegeController,
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'College',
+                      prefixIcon: Icon(Icons.school, color: Colors.white),
+                      hintStyle: TextStyle(color: Colors.grey[700]),
+                    ),
+                  ),
+                ),
+                SizedBox(height: _boxh),
+                Container(
+                  height: 40,
+                  margin: EdgeInsets.only(left: 40, right: 40),
+                  child: TextFormField(
+                    controller: _subjectController,
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    decoration: InputDecoration(
+                      hintText: 'Number of Subjects',
+                      prefixIcon: Icon(
+                          Icons.library_books, color: Colors.white),
+                      hintStyle: TextStyle(color: Colors.grey[700]),
+                    ),
+                  ),
+                ),
+                SizedBox(height: _boxh),
+                Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    TextFormField(
-                      controller: _displayName,
-                      decoration: const InputDecoration(labelText: 'Full Name'),
-                      validator: (String value) {
-                        if (value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      controller: _emailController,
-                      decoration: const InputDecoration(labelText: 'Email'),
-                      validator: (String value) {
-                        if (value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      controller: _passwordController,
-                      decoration: const InputDecoration(labelText: 'Password'),
-                      validator: (String value) {
-                        if (value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      },
-                      obscureText: true,
-                    ),
-                    TextFormField(
-                      controller: _collegeController,
-                      decoration:
-                          const InputDecoration(labelText: 'College Name'),
-                      validator: (String value) {
-                        if (value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      controller: _subjectController,
-                      decoration: const InputDecoration(
-                          labelText: 'Total no. of Subjects'),
-                      validator: (String value) {
-                        if (value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      },
-                    ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(vertical: 16.0),
-                      alignment: Alignment.center,
-                      child: OutlineButton(
-                        child: Text("register"),
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            _registerAccount();
-                          }
-                        },
-                      ),
-                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          child: RaisedButton(
+                            elevation: 5,
+                            onPressed: () => print('Login pressed'),
+                            padding: EdgeInsets.all(15),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)
+                            ),
+                            color: Colors.white,
+                            child: Text(
+                              'LOGIN',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: _boxh),
+                        Container(
+                          child: RaisedButton(
+                            elevation: 5,
+                            onPressed: () => _registerAccount(),
+                            padding: EdgeInsets.all(15),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)
+                            ),
+                            color: Colors.white,
+                            child: Text(
+                              'REGISTER',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
                   ],
                 ),
-              ),
-            )),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -145,7 +211,7 @@ class _RegisterState extends State<Register> {
                 user: user1,
               )));
     } else {
-      _isSuccess = false;
+      print("User not registered");
     }
   }
 }
--- lib/forms/add-asnmt.dart
+++ lib/forms/add-asnmt.dart
@@ -43,169 +43,171 @@ class  AddAsnmtState extends State <AddAsnmt> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Scaffold(
-            backgroundColor: Colors.white,
-          ),
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('media/bg-app.png'),
-                fit: BoxFit.cover,
+    return Expanded(
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Scaffold(
+              backgroundColor: Colors.white,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('media/bg-app.png'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(Icons.assignment,
-                  size: 60,
-                ),
-                Text(
-                  "ADD ASSIGNMENT",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.assignment,
+                    size: 60,
                   ),
-                ),
-                SizedBox(height: _boxh*2),
-                Container(
-                  height: 50,
-                  margin: EdgeInsets.only(left:40,right:40),
-                  child: TextFormField(
-                    controller: _titleController,
-                    style: TextStyle(fontSize: 16, color: Colors.white),
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.assignment,
-                        color: Colors.white,
-                      ),
-                      hintText: 'Title/Description',
-                      hintStyle: TextStyle(color: Colors.grey[700]),
-                      fillColor: Colors.white.withOpacity(0.9),
+                  Text(
+                    "ADD ASSIGNMENT",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                SizedBox(height: _boxh*2),
-                Row(
-                  children: <Widget>[
-                    Container(
-                      height: 50,
-                      margin: EdgeInsets.only(left:40,right:40),
-                      child: DropdownButton(
-                        hint: Text(
-                          'Select Subject',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                        dropdownColor: Colors.white,
-                        focusColor: Colors.white,
-                        icon: Icon(Icons.arrow_drop_down,
+                  SizedBox(height: _boxh*2),
+                  Container(
+                    height: 50,
+                    margin: EdgeInsets.only(left:40,right:40),
+                    child: TextFormField(
+                      controller: _titleController,
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.assignment,
                           color: Colors.white,
                         ),
-                        iconSize: 36,
-                        value: _subjVal,
-                        onChanged:(value){
-                          setState(() {
-                            _subjVal = value;
-                          });
-                        },
-                        items: _subjName.map((value) {
-                          return DropdownMenuItem(
-                            value: value,
-                            child: Text(value,
-                              style: TextStyle(
-                                color: Colors.grey[700],
-                              ),
-                            ),
-                          );
-                        }).toList(),
+                        hintText: 'Title/Description',
+                        hintStyle: TextStyle(color: Colors.grey[700]),
+                        fillColor: Colors.white.withOpacity(0.9),
                       ),
                     ),
-                    Column(
-                      children: <Widget>[
-                        Container(
-                          height: 40,
-                          child: RaisedButton(
-                            color: Colors.grey,
-                            child: Text(
-                              _dateTime != null ? 'Pick another date' : 'Enter Submission Date',
-                              style: TextStyle(
-                                color: Colors.grey[800],
-                              ),
-                            ),
-                            onPressed: (){
-                              showDatePicker(
-                                context: context,
-                                initialDate: _dateTime == null ? DateTime.now() : _dateTime,
-                                firstDate: DateTime(2020),
-                                lastDate: DateTime(2025),
-                                initialEntryMode: DatePickerEntryMode.input,
-                              ).then((date){
-                                setState(() {
-                                  _dateTime = date;
-                                  _formattedDT = DateFormat('dd-MM-yyyy').format(_dateTime);
-                                });
-                              });
-                            },
-                          ),
-                        ),
-                        Container(
-                          child: Text(
-                            _dateTime == null ? 'No date has been picked yet!' : _formattedDT,
+                  ),
+                  SizedBox(height: _boxh*2),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        height: 50,
+                        margin: EdgeInsets.only(left:40,right:40),
+                        child: DropdownButton(
+                          hint: Text(
+                            'Select Subject',
                             style: TextStyle(
                               color: Colors.white,
                             ),
                           ),
+                          dropdownColor: Colors.white,
+                          focusColor: Colors.white,
+                          icon: Icon(Icons.arrow_drop_down,
+                            color: Colors.white,
+                          ),
+                          iconSize: 36,
+                          value: _subjVal,
+                          onChanged:(value){
+                            setState(() {
+                              _subjVal = value;
+                            });
+                          },
+                          items: _subjName.map((value) {
+                            return DropdownMenuItem(
+                              value: value,
+                              child: Text(value,
+                                style: TextStyle(
+                                  color: Colors.grey[700],
+                                ),
+                              ),
+                            );
+                          }).toList(),
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(height: _boxh),
-                Container(
-                  height: 40,
-                  margin: EdgeInsets.only(left:40,right:40),
-                  child: ListTile(
-                    title: Text(
-                      _time!=null ? 'Submission Deadline: ${_time.hour} : ${_time.minute} ${_time.period.toString().substring(10,12)}' : 'Enter Time of Deadline',
-                      style: TextStyle(
-                        color: _time!=null ? Colors.white : Colors.grey[700],
                       ),
-                    ),
-                    leading: Icon(Icons.access_time,
-                      color: Colors.white,
-                    ),
-                    onTap: () {
-                      selectTime(context);
-                      print(_time);
-                    },
-                  ),
-                ),
-                SizedBox(height: 30,),
-                Container(
-                  height: 40,
-                  margin: EdgeInsets.only(left:40,right:40),
-                  color: Colors.white,
-                  child: OutlineButton(
-                    child: Text(
-                      "ADD ASSIGNMENT",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            height: 40,
+                            child: RaisedButton(
+                              color: Colors.grey,
+                              child: Text(
+                                _dateTime != null ? 'Pick another date' : 'Enter Submission Date',
+                                style: TextStyle(
+                                  color: Colors.grey[800],
+                                ),
+                              ),
+                              onPressed: (){
+                                showDatePicker(
+                                  context: context,
+                                  initialDate: _dateTime == null ? DateTime.now() : _dateTime,
+                                  firstDate: DateTime(2020),
+                                  lastDate: DateTime(2025),
+                                  initialEntryMode: DatePickerEntryMode.input,
+                                ).then((date){
+                                  setState(() {
+                                    _dateTime = date;
+                                    _formattedDT = DateFormat('dd-MM-yyyy').format(_dateTime);
+                                  });
+                                });
+                              },
+                            ),
+                          ),
+                          Container(
+                            child: Text(
+                              _dateTime == null ? 'No date has been picked yet!' : _formattedDT,
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    onPressed: () => _addAssignment(),
+                    ],
                   ),
-                ),
-              ],
+                  SizedBox(height: _boxh),
+                  Container(
+                    height: 40,
+                    margin: EdgeInsets.only(left:40,right:40),
+                    child: ListTile(
+                      title: Text(
+                        _time!=null ? 'Submission Deadline: ${_time.hour} : ${_time.minute} ${_time.period.toString().substring(10,12)}' : 'Enter Time of Deadline',
+                        style: TextStyle(
+                          color: _time!=null ? Colors.white : Colors.grey[700],
+                        ),
+                      ),
+                      leading: Icon(Icons.access_time,
+                        color: Colors.white,
+                      ),
+                      onTap: () {
+                        selectTime(context);
+                        print(_time);
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Container(
+                    height: 40,
+                    margin: EdgeInsets.only(left:40,right:40),
+                    color: Colors.white,
+                    child: OutlineButton(
+                      child: Text(
+                        "ADD ASSIGNMENT",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                      onPressed: () => _addAssignment(),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
